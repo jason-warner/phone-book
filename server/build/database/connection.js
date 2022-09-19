d4008026641fcfd7fbf60353c9fb311ab1519298
jason-warner <thupermn@protonmail.com>
2022-09-19 13:37:55 -0400

    figured out Docker DB connection issue, everything works with one command using docker compose!

--- server/build/database/connection.js
+++ server/build/database/connection.js
@@ -12,13 +12,10 @@ const password = encodeURIComponent(String(process.env.ME_CONFIG_MONGODB_ADMINPA
 const host = encodeURIComponent(String(process.env.MONGO_INITDB_HOST));
 const port = encodeURIComponent(String(process.env.MONGO_INITDB_PORT));
 const dbURI = `mongodb://${username}:${password}@${host}:${port}`;
-// TEMP REMOTE DB URI
-// const dbURI = `mongodb+srv://${username}:${password}@cluster0.w3l3h.gcp.mongodb.net/phone-book?retryWrites=true&w=majority`;
-console.log('db URI: ', dbURI);
 const mongoConnection = async () => {
     try {
         await mongoose_1.default.connect(dbURI)
-            .then(() => console.log('Connected to the database.'));
+            .then(() => console.log(`Connected to the database @ ${dbURI}`));
     }
     catch (error) {
         console.error('Database Connection Error: ', error);
--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -11,17 +11,10 @@ const port = encodeURIComponent(String(process.env.MONGO_INITDB_PORT));
 
 const dbURI = `mongodb://${username}:${password}@${host}:${port}`;
 
-// TEMP REMOTE DB URI
-// const dbURI = `mongodb+srv://${username}:${password}@cluster0.w3l3h.gcp.mongodb.net/phone-book?retryWrites=true&w=majority`;
-
-
-console.log('db URI: ', dbURI);
-
-
 export const mongoConnection = async () => {
     try {
         await mongoose.connect(dbURI)
-            .then(() => console.log('Connected to the database.'))
+            .then(() => console.log(`Connected to the database @ ${dbURI}`))
     } catch (error) {
         console.error('Database Connection Error: ', error);
     }
